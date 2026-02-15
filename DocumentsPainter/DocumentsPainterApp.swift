8d7f1c1f22bc8a88a0d4e7190baa71a4156def81
romanborysenkov <albertstraustrup@gmail.com>
2026-02-16 01:57:09 +0200

    Initial Commit

---
+++ DocumentsPainter/DocumentsPainterApp.swift
@@ -0,0 +1,17 @@
+//
+//  DocumentsPainterApp.swift
+//  DocumentsPainter
+//
+//  Created by Romsya Borysenko on 2/16/26.
+//
+
+import SwiftUI
+
+@main
+struct DocumentsPainterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DocumentsPainterTests/DocumentsPainterTests.swift
@@ -0,0 +1,17 @@
+//
+//  DocumentsPainterTests.swift
+//  DocumentsPainterTests
+//
+//  Created by Romsya Borysenko on 2/16/26.
+//
+
+import Testing
+@testable import DocumentsPainter
+
+struct DocumentsPainterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
